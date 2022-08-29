7b453226cb9f124688ff51e291ef359179a9af6d
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-29 16:38:23 -0300

    Item menu datasource contract

---
+++ lib/src/modules/menu/infra/datasources/i_item_menu_datasource.dart
@@ -0,0 +1,11 @@
+import 'package:cardapio_manager/src/modules/menu/infra/models/item_menu_model.dart';
+
+abstract class IItemMenuDatasource {
+  Future<ItemMenuModel> create(ItemMenuModel item);
+
+  Future<ItemMenuModel> update(ItemMenuModel item);
+
+  Future<bool> disable(String id);
+
+  Future<List<ItemMenuModel>> findAll();
+}
