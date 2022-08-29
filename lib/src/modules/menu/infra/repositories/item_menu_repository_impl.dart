cc931c2166e914c832ce69e48a8669a80e8f3538
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-29 16:39:02 -0300

    Implementation of create method

---
+++ lib/src/modules/menu/infra/repositories/item_menu_repository_impl.dart
@@ -0,0 +1,42 @@
+import 'package:cardapio_manager/src/modules/menu/domain/entities/item_menu.dart';
+import 'package:cardapio_manager/src/modules/menu/domain/repositories/i_item_menu_repository.dart';
+import 'package:cardapio_manager/src/modules/menu/errors/item_menu_errors.dart';
+import 'package:cardapio_manager/src/modules/menu/infra/datasources/i_item_menu_datasource.dart';
+import 'package:cardapio_manager/src/modules/menu/infra/models/item_menu_model.dart';
+import 'package:dartz/dartz.dart';
+
+class ItemMenuRepositoryImpl implements IItemMenuRepository {
+  final IItemMenuDatasource _datasource;
+
+  ItemMenuRepositoryImpl(this._datasource);
+
+  @override
+  Future<Either<ItemMenuError, ItemMenu>> create(ItemMenu item) async {
+    try {
+      final result =
+          await _datasource.create(ItemMenuModel.fromItemMenu(item: item));
+
+      return Right(result);
+    } catch (e) {
+      return Left(ItemMenuError(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<ItemMenuError, ItemMenu>> update(ItemMenu item) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ItemMenuError, bool>> disable(String id) {
+    // TODO: implement disable
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ItemMenuError, List<ItemMenu>>> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+}
