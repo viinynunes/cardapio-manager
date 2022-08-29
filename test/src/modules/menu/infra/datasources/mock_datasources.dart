dd50af7f71d911275043e8f8bfa1eb23172e4723
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-29 16:39:19 -0300

    Mocks to datasource

---
+++ test/src/modules/menu/infra/datasources/mock_datasources.dart
@@ -0,0 +1,5 @@
+import 'package:cardapio_manager/src/modules/menu/infra/datasources/i_item_menu_datasource.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([IItemMenuDatasource])
+main() {}
