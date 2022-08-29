0a74fa68cae8721113d07cea74a92dfdf30efb1b
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-29 13:57:36 -0300

    Mocks generated by build_runner

---
+++ test/src/modules/menu/domain/repositories_mocks.dart
@@ -0,0 +1,5 @@
+import 'package:cardapio_manager/src/modules/menu/domain/repositories/i_item_menu_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([IItemMenuRepository])
+main() {}
