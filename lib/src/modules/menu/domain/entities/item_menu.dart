decd2532c2c602539b91453ec712176392391a65
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-30 14:48:33 -0300

    Making imgFile nullable and fixing a default imgUrl

--- lib/src/modules/menu/domain/entities/item_menu.dart
+++ lib/src/modules/menu/domain/entities/item_menu.dart
@@ -1,17 +1,23 @@
+import 'dart:io';
+
 class ItemMenu {
   String? id;
   String name;
   String description;
-  String imgUrl;
+  String imgUrl =
+      'https://firebasestorage.googleapis.com/v0/b/cardapio-app-39905.appspot.com/o/images%2Fmenu-no-picture.jpg?alt=media&token=9f3b9daf-c3e7-4c00-bb16-e8851b5699f7';
   bool enabled;
 
   List<int> weekdayList;
+  File? imgFile;
 
-  ItemMenu(
-      {required this.id,
-      required this.name,
-      required this.description,
-      required this.imgUrl,
-      required this.enabled,
-      required this.weekdayList});
+  ItemMenu({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imgUrl,
+    required this.enabled,
+    required this.weekdayList,
+    this.imgFile,
+  });
 }
