0b72d8136de8ef180d9d60a8c4bab7d2cdd275c3
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-30 22:25:40 -0300

    class URLS with link of item menu empty image

---
+++ lib/src/modules/menu/presenter/utils/utils.dart
@@ -0,0 +1,4 @@
+class URLS {
+  static const itemMenuNoImageUrl =
+      'https://firebasestorage.googleapis.com/v0/b/cardapio-app-39905.appspot.com/o/images%2Fmenu-no-picture.jpg?alt=media&token=9f3b9daf-c3e7-4c00-bb16-e8851b5699f7';
+}
