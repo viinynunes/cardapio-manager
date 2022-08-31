d6ea38c871e55bb7814f83cf01edda7ff10903a2
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-30 22:26:11 -0300

    Removed imgUrl from entity

--- lib/src/modules/menu/domain/entities/item_menu.dart
+++ lib/src/modules/menu/domain/entities/item_menu.dart
@@ -4,8 +4,7 @@ class ItemMenu {
   String? id;
   String name;
   String description;
-  String imgUrl =
-      'https://firebasestorage.googleapis.com/v0/b/cardapio-app-39905.appspot.com/o/images%2Fmenu-no-picture.jpg?alt=media&token=9f3b9daf-c3e7-4c00-bb16-e8851b5699f7';
+  String imgUrl;
   bool enabled;
 
   List<int> weekdayList;
