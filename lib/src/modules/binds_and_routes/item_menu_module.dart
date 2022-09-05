9d923f91732871295c691246042e8bd3fc40d022
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-05 15:38:54 -0300

    Added Days of week Dependencies

--- lib/src/modules/binds_and_routes/item_menu_module.dart
+++ lib/src/modules/binds_and_routes/item_menu_module.dart
@@ -1,6 +1,8 @@
+import 'package:cardapio_manager/src/modules/menu/domain/usecases/impl/days_of_week_usecase_impl.dart';
 import 'package:cardapio_manager/src/modules/menu/domain/usecases/impl/item_menu_usecase_impl.dart';
 import 'package:cardapio_manager/src/modules/menu/external/menu_firebase_datasource_impl.dart';
 import 'package:cardapio_manager/src/modules/menu/infra/repositories/item_menu_repository_impl.dart';
+import 'package:cardapio_manager/src/modules/menu/presenter/bloc/days_of_week_bloc.dart';
 import 'package:cardapio_manager/src/modules/menu/presenter/bloc/item_menu_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -10,6 +12,11 @@ import '../menu/presenter/pages/item_menu_page.dart';
 class ItemMenuModule extends Module {
   @override
   List<Bind> get binds => [
+        //Days of week Dependencies
+        Bind((i) => DaysOfWeekUsecaseImpl()),
+        Bind((i) => DaysOfWeekBloc(i())),
+
+        //ItemMenu Dependencies
         Bind((i) => MenuFirebaseDatasourceImpl()),
         Bind((i) => ItemMenuRepositoryImpl(i())),
         Bind((i) => ItemMenuUsecaseImpl(i())),
