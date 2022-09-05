db50d81710a13d020bea80a6aa851ef9d4518244
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-05 15:36:44 -0300

    Moved Animated Container to a different class and build animation

--- lib/src/modules/menu/presenter/pages/widgets/item_menu_principal_image_container.dart
+++ lib/src/modules/menu/presenter/pages/widgets/item_menu_principal_image_container.dart
@@ -7,12 +7,16 @@ import 'package:flutter_modular/flutter_modular.dart';
 import '../../../../core/camera/presenter/bloc/camera_bloc.dart';
 import '../../../../core/camera/presenter/bloc/events/camera_events.dart';
 import '../../../../core/camera/presenter/bloc/states/camera_states.dart';
+import '../../../../core/camera/presenter/pages/camera_source_animated_selector.dart';
 import '../../utils/utils.dart';
 import 'file_image_container.dart';
 import 'network_image_container.dart';
 
 class ItemMenuPrincipalImageContainer extends StatefulWidget {
-  const ItemMenuPrincipalImageContainer({Key? key}) : super(key: key);
+  const ItemMenuPrincipalImageContainer({Key? key, required this.getFileImage})
+      : super(key: key);
+
+  final Function(File? file) getFileImage;
 
   @override
   State<ItemMenuPrincipalImageContainer> createState() =>
@@ -20,7 +24,8 @@ class ItemMenuPrincipalImageContainer extends StatefulWidget {
 }
 
 class _ItemMenuPrincipalImageContainerState
-    extends State<ItemMenuPrincipalImageContainer> with SingleTickerProviderStateMixin{
+    extends State<ItemMenuPrincipalImageContainer>
+    with SingleTickerProviderStateMixin {
   final cameraBloc = Modular.get<CameraBloc>();
 
   late File image;
@@ -32,14 +37,10 @@ class _ItemMenuPrincipalImageContainerState
   void initState() {
     super.initState();
 
-    cameraSourceAnimatedController = AnimationController(vsync: this);
-    cameraSourceAnimatedController.duration = const Duration(milliseconds: 0);
+    cameraSourceAnimatedController = AnimationController(vsync: this)
+      ..duration = const Duration(milliseconds: 0);
     cameraSourceAnimation = Tween<double>(begin: 0, end: 100)
         .animate(cameraSourceAnimatedController);
-
-    cameraSourceAnimation.addStatusListener((status) {
-      setState(() {});
-    });
   }
 
   @override
@@ -53,12 +54,12 @@ class _ItemMenuPrincipalImageContainerState
         children: [
           BlocBuilder<CameraBloc, CameraStates>(
             bloc: cameraBloc,
-            builder: (_, state) {
+            builder: (context, state) {
               if (state is CameraSuccessState) {
                 image = state.file;
+                widget.getFileImage(image);
                 return FileImageContainer(file: image);
               }
-
               return const NetworkImageContainer(
                 imagePath: URLS.itemMenuNoImageUrl,
               );
@@ -66,72 +67,15 @@ class _ItemMenuPrincipalImageContainerState
           ),
           Align(
             alignment: Alignment.bottomRight,
-            child: Container(
-                padding: const EdgeInsets.only(right: 4),
-                width: size.width * 0.4,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    AnimatedContainer(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Theme.of(context).secondaryHeaderColor),
-                      curve: Curves.elasticOut,
-                      width: 50,
-                      height: cameraSourceAnimation.value,
-                      duration: const Duration(seconds: 1),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.black38),
-                        child: Column(
-                          children: [
-                            Flexible(
-                              child: IconButton(
-                                onPressed: () {
-                                  cameraBloc
-                                      .add(PickImageEvent(imageFrom: 'camera'));
-                                },
-                                icon: const Icon(
-                                  Icons.camera_alt,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            Flexible(
-                              child: IconButton(
-                                onPressed: () {
-                                  cameraBloc.add(
-                                      PickImageEvent(imageFrom: 'gallery'));
-                                },
-                                icon: const Icon(
-                                  Icons.file_upload,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        cameraSourceAnimatedController.forward();
-                      },
-                      style: ElevatedButton.styleFrom(
-                          primary:
-                              Theme.of(context).primaryColor.withOpacity(0.8)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: const [
-                          Text('Alterar Foto'),
-                          Icon(Icons.camera_alt_rounded),
-                        ],
-                      ),
-                    ),
-                  ],
-                )),
+            child: CameraSourceAnimatedSelector(
+              animation: cameraSourceAnimation,
+              onForward: () => cameraSourceAnimatedController.forward(),
+              onReverse: () => cameraSourceAnimatedController.reverse(),
+              onGallery: () =>
+                  cameraBloc.add(PickImageEvent(imageFrom: 'gallery')),
+              onCamera: () =>
+                  cameraBloc.add(PickImageEvent(imageFrom: 'camera')),
+            ),
           ),
         ],
       ),
