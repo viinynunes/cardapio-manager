e9df98184913e50d7a7eaede67b49fabd2c95c23
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-06 11:11:18 -0300

    Create method implemented

--- lib/src/modules/menu/external/menu_firebase_datasource_impl.dart
+++ lib/src/modules/menu/external/menu_firebase_datasource_impl.dart
@@ -1,3 +1,4 @@
+import 'package:cardapio_manager/src/modules/menu/errors/item_menu_errors.dart';
 import 'package:cardapio_manager/src/modules/menu/infra/datasources/i_item_menu_datasource.dart';
 import 'package:cardapio_manager/src/modules/menu/infra/models/item_menu_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,23 +6,34 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 class MenuFirebaseDatasourceImpl implements IItemMenuDatasource {
   final _menuCollection = FirebaseFirestore.instance.collection('menu');
-  final _menuStorage = FirebaseStorage.instance.ref().child('images');
+  final _menuStorage = FirebaseStorage.instance.ref();
 
   @override
   Future<ItemMenuModel> create(ItemMenuModel item) async {
-/*    final result = await _menuCollection.add(item.toMap()).then((value) {
+    try {
+      if (item.imgFile != null) {
+        final recTask = _menuStorage
+            .child('images/menu/${item.name}')
+            .putFile(item.imgFile!);
+
+        final snap = await recTask.whenComplete(() {});
+
+        item.imgUrl = await snap.ref.getDownloadURL();
+      }
+    } on FirebaseException catch (e) {
+      throw ItemMenuError(e.message.toString());
+    }
+
+    await _menuCollection.add(item.toMap()).then((value) {
       item.id = value.id;
       _menuCollection.doc(item.id).update(item.toMap());
     }).catchError((e) => throw Exception(e.toString()));
 
-    return item;*/
-    // TODO: implement update
-    throw UnimplementedError();
+    return item;
   }
 
   @override
   Future<ItemMenuModel> update(ItemMenuModel item) {
-    // TODO: implement update
     throw UnimplementedError();
   }
 
