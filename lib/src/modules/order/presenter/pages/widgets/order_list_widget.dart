9a3fad36ad7663b0013fd7f3475e6ab456fdeb89
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-19 16:12:39 -0300

    First layout

---
+++ lib/src/modules/order/presenter/pages/widgets/order_list_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../bloc/events/order_events.dart';
+import '../../bloc/order_bloc.dart';
+import '../../bloc/states/order_states.dart';
+import '../tiles/orders_tile.dart';
+
+class OrderListWidget extends StatefulWidget {
+  const OrderListWidget({Key? key, required this.day}) : super(key: key);
+
+  final DateTime day;
+
+  @override
+  State<OrderListWidget> createState() => _OrderListWidgetState();
+}
+
+class _OrderListWidgetState extends State<OrderListWidget> {
+  final bloc = Modular.get<OrderBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    bloc.add(GetOrdersByDayEvent(widget.day));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
+      height: size.height * 0.8,
+      width: size.width,
+      child: BlocBuilder<OrderBloc, OrderStates>(
+        bloc: bloc,
+        builder: (_, state) {
+          if (state is OrderGetListSuccessState) {
+            final orderList = state.orderList;
+
+            if (orderList.isEmpty) {
+              return const Center(
+                child: Text('Sem pedidos para a data selecionada'),
+              );
+            }
+            return ListView.builder(
+                itemCount: orderList.length,
+                itemBuilder: (_, index) {
+                  var order = orderList[index];
+                  return OrdersTile(order: order, onTap: () {});
+                });
+          }
+
+          if (state is OrderLoadingState) {
+            return Overlay(
+              initialEntries: [
+                OverlayEntry(builder: (_) {
+                  return Container(
+                    color: Colors.black26,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                })
+              ],
+            );
+          }
+
+          if (state is OrderErrorState) {
+            return Center(
+              child: Text(state.orderError.message),
+            );
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
