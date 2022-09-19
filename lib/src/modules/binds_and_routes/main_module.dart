86151e3835186d0b18551f78e062928311ff1cdb
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-19 16:59:11 -0300

    Added Days of week Dependencies

--- lib/src/modules/binds_and_routes/main_module.dart
+++ lib/src/modules/binds_and_routes/main_module.dart
@@ -4,9 +4,17 @@ import 'package:cardapio_manager/src/modules/core/camera/presenter/bloc/camera_b
 import 'package:cardapio_manager/src/modules/core/camera/services/impl/image_picker_service.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../core/weekday/domain/usecases/impl/days_of_week_usecase_impl.dart';
+import '../core/weekday/presenter/bloc/days_of_week_bloc.dart';
+
 class MainModule extends Module {
   @override
   List<Bind> get binds => [
+        //Days of week Dependencies
+        Bind((i) => DaysOfWeekUsecaseImpl()),
+        Bind((i) => DaysOfWeekBloc(i())),
+
+        //ImageDriver Dependencies
         Bind((i) => ImagePickerService()),
         Bind.factory((i) => CameraBloc(i())),
       ];
