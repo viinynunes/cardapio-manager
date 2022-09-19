7c0b2d19d6550f2ae127afa0ad90683573e5fba0
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-19 16:10:34 -0300

    First layout

--- lib/src/modules/order/presenter/pages/tiles/orders_tile.dart
+++ lib/src/modules/order/presenter/pages/tiles/orders_tile.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 import '../../../domain/entities/enums/order_status_enum.dart';
 import '../../../domain/entities/order.dart';
 
-class OrdersTile extends StatelessWidget {
+class OrdersTile extends StatefulWidget {
   const OrdersTile({Key? key, required this.order, required this.onTap})
       : super(key: key);
 
@@ -12,48 +12,109 @@ class OrdersTile extends StatelessWidget {
   final VoidCallback onTap;
 
   @override
+  State<OrdersTile> createState() => _OrdersTileState();
+}
+
+class _OrdersTileState extends State<OrdersTile> {
+  @override
   Widget build(BuildContext context) {
     final dateFormat = DateFormat('dd/MM/yyyy');
     final size = MediaQuery.of(context).size;
 
-    return Container(
-      height: size.height * 0.1,
+    double height = size.height * 0.1;
+
+    return AnimatedContainer(
+      height: height,
+      duration: const Duration(seconds: 1),
       decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(0.3),
         borderRadius: BorderRadius.circular(16),
       ),
       child: GestureDetector(
-        onTap: onTap,
+        onTap: widget.onTap,
         child: Card(
           child: Row(
             children: [
               Flexible(
-                flex: 1,
+                flex: 2,
                 fit: FlexFit.tight,
                 child: Text(
-                  dateFormat.format(order.registrationDate),
+                  dateFormat.format(widget.order.registrationDate),
                   textAlign: TextAlign.center,
                 ),
               ),
               Flexible(
-                flex: 3,
+                flex: 4,
                 fit: FlexFit.tight,
-                child: Text(
-                  order.client.name,
-                  textAlign: TextAlign.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Text(
+                        widget.order.id,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Text(
+                        widget.order.client.name,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: ListView(
+                        children: widget.order.menuList
+                            .map((e) => Text(
+                                  e.name,
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                      color: Theme.of(context).primaryColor),
+                                ))
+                            .toList(),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Flexible(
-                flex: 1,
+                flex: 2,
                 fit: FlexFit.tight,
-                child: Text(
-                  order.status.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: order.status.name == OrderStatus.cancelled.name
-                          ? Colors.red
-                          : Colors.green),
-                ),
+                child: widget.order.status == OrderStatus.open
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            widget.order.status.name,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: widget.order.status.name ==
+                                        OrderStatus.cancelled.name
+                                    ? Colors.red
+                                    : Colors.green),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Confirmar',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Text(
+                        widget.order.status.name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: widget.order.status.name ==
+                                    OrderStatus.cancelled.name
+                                ? Colors.red
+                                : Colors.green),
+                      ),
               ),
             ],
           ),
