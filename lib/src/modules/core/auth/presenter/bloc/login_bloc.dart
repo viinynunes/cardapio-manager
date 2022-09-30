7572b2e4c48eb417ea8645c3a59c271a198f358d
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-09-30 16:05:55 -0300

    Calling SaveLoggedUserEvent after make firebase login

--- lib/src/modules/core/auth/presenter/bloc/login_bloc.dart
+++ lib/src/modules/core/auth/presenter/bloc/login_bloc.dart
@@ -1,19 +1,24 @@
 import 'package:cardapio_manager/src/modules/core/auth/domain/usecases/i_login_usecase.dart';
+import 'package:cardapio_manager/src/modules/core/auth/presenter/bloc/events/logged_user_events.dart';
 import 'package:cardapio_manager/src/modules/core/auth/presenter/bloc/events/login_events.dart';
 import 'package:cardapio_manager/src/modules/core/auth/presenter/bloc/states/login_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'logged_user_bloc.dart';
+
 class LoginBloc extends Bloc<LoginEvents, LoginStates> {
   final ILoginUsecase loginUsecase;
+  final LoggedUserBloc loggedUserBloc;
 
-  LoginBloc(this.loginUsecase) : super(LoginIdleState()) {
+  LoginBloc(this.loginUsecase, this.loggedUserBloc) : super(LoginIdleState()) {
     on<LoginEvent>((event, emit) async {
       emit(LoginLoadingState());
 
       final result = await loginUsecase.login(event.email, event.password);
 
-      result.fold(
-          (l) => emit(LoginErrorState(l)), (r) => emit(LoginSuccessState(r)));
+      result.fold((l) => emit(LoginErrorState(l)), (user) async {
+        loggedUserBloc.add(SaveLoggedUserEvent(user));
+      });
     });
   }
 }
