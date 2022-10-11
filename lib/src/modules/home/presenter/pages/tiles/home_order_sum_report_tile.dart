eba15c62a5065100b342c51ea42b9881dbd2b249
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-10-11 20:49:51 -0300

    tile order sum from home

---
+++ lib/src/modules/home/presenter/pages/tiles/home_order_sum_report_tile.dart
@@ -0,0 +1,44 @@
+import 'package:cardapio_manager/src/modules/core/reports/domain/entities/order_sum_report.dart';
+import 'package:flutter/material.dart';
+
+class HomeOrderReportSumReportTile extends StatelessWidget {
+  const HomeOrderReportSumReportTile(
+      {Key? key, required this.report, required this.color})
+      : super(key: key);
+
+  final OrderSumReport report;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Container(
+              color: color,
+              height: size.height * 0.01,
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Flexible(
+            flex: 9,
+            fit: FlexFit.tight,
+            child: Text(
+              report.itemName,
+              style: TextStyle(color: color, fontSize: 12),
+              maxLines: 3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
