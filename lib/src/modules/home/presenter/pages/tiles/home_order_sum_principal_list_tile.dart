f13cde162340c2a2815f8089e6b69772d6a9e181
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-10-14 16:13:01 -0300

    OnTap function added

--- lib/src/modules/home/presenter/pages/tiles/home_order_sum_principal_list_tile.dart
+++ lib/src/modules/home/presenter/pages/tiles/home_order_sum_principal_list_tile.dart
@@ -2,37 +2,46 @@ import 'package:cardapio_manager/src/modules/core/reports/domain/entities/order_
 import 'package:flutter/material.dart';
 
 class HomeOrderSumPrincipalListTile extends StatelessWidget {
-  const HomeOrderSumPrincipalListTile({Key? key, required this.report})
+  const HomeOrderSumPrincipalListTile({Key? key, required this.report, required this.onTap})
       : super(key: key);
 
   final OrderSumReport report;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Text(
-              report.itemName,
-              style: const TextStyle(fontSize: 16),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              flex: 3,
+              fit: FlexFit.tight,
+              child: Text(
+                report.itemName,
+                style: const TextStyle(fontSize: 16),
+              ),
             ),
-          ),
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Text(report.totalSumOrders.toString(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 16)),
-          ),
-        ],
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Text(report.totalSumOrders.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 16)),
+            ),
+            const Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Icon(Icons.list),
+            ),
+          ],
+        ),
       ),
     );
   }
